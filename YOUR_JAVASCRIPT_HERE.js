5853fba3402e1d47ce135a1b6b1f639b98d304a5
EWLJanssenE <ewljanssen@gmail.com>
2019-07-05 20:27:00 +0200

    first part, 6 steps

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,11 @@
+// Write your JS here
+const hero  = {
+    name: "henk",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "scissors",
+        damage: 2,
+    },
+}
